ddb1fd90dc4db555093151537c2a487730411818
TrixxedHeart <46364955+TrixxedHeart@users.noreply.github.com>
2026-04-20 12:46:21 -0500

    Remove comment referring to flag that doesn't exist

--- Content.Shared/_ST/CCVar/CCVars.Particles.cs
+++ Content.Shared/_ST/CCVar/CCVars.Particles.cs
@@ -10,8 +10,6 @@ public sealed partial class CCVars
     /// Low:    25% of maxCount per emitter
     /// Medium: 50% of maxCount per emitter
     /// High:   100% of maxCount per emitter
-    ///
-    /// Note: Particles with IgnoreQualitySettings = true always render at full quality.
     /// </summary>
     public static readonly CVarDef<int> ParticleQuality =
         CVarDef.Create("particles.quality", 3, CVar.CLIENTONLY | CVar.ARCHIVE);
